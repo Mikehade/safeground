1237c2f3f05ef42c25d0ff59bf9257ab7d46b4d3
Michael Adeyeri <adeyerimichael0@gmail.com>
2026-09-18 19:13:45 +0100

    update compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,39 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5173,
+//     host: '0.0.0.0',
+//     proxy: {
+//       '/api': {
+//         // target: 'http://localhost:8000',
+//         target: process.env.VITE_API_PROXY_TARGET || "http://localhost:8000",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// })
+
+
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      host: '0.0.0.0',
+      proxy: {
+        '/api': {
+          target: env.VITE_API_PROXY_TARGET || 'http://localhost:8000',
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 })
